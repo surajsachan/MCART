beff66d8cc0eff51e81c3a1b6e61eac946c0a064
surajsachan <surajsachan@gmail.com>
2025-04-14 03:34:43 +0530

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,58 +1,75 @@
-import React from 'react'
-import { Footer, Navbar } from "../components";
-import { Link } from 'react-router-dom';
-const Register = () => {
+import React, { useState, useContext } from 'react';
+import authService from '../services/auth.service';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+import { Form, Container, FloatingLabel, Button } from "react-bootstrap";
+
+function RegisterForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage('');
+        setLoading(true)
+        try {
+            await authService.register(email, password);
+            await authService.login(email, password);
+            login(await authService.getCurrentUser());
+            navigate('/');
+        } catch (error) {
+            setMessage(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <>
-            <Navbar />
-            <div className="container my-3 py-3">
-                <h1 className="text-center">Register</h1>
-                <hr />
-                <div class="row my-4 h-100">
-                    <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-                        <form>
-                            <div class="form my-3">
-                                <label for="Name">Full Name</label>
-                                <input
-                                    type="email"
-                                    class="form-control"
-                                    id="Name"
-                                    placeholder="Enter Your Name"
-                                />
-                            </div>
-                            <div class="form my-3">
-                                <label for="Email">Email address</label>
-                                <input
-                                    type="email"
-                                    class="form-control"
-                                    id="Email"
-                                    placeholder="name@example.com"
-                                />
-                            </div>
-                            <div class="form  my-3">
-                                <label for="Password">Password</label>
-                                <input
-                                    type="password"
-                                    class="form-control"
-                                    id="Password"
-                                    placeholder="Password"
-                                />
-                            </div>
-                            <div className="my-3">
-                                <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
-                            </div>
-                            <div className="text-center">
-                                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
-                                    Register
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+        <Container style={{ width: "400px" }}>
+            <div className="p-4 box">
+                <h2 className="mb-3">Signup</h2>
+                <Form onSubmit={handleSubmit}>
+
+                <FloatingLabel
+                    controlId="formBasicName"
+                    label="Name"
+                    className="mb-3"
+                >
+                    <Form.Control type="text" placeholder="John Doe" onChange={(e) => setName(e.target.value)} />
+                </FloatingLabel>
+
+                <FloatingLabel
+                    controlId="formBasicEmail"
+                    label="Email address"
+                    className="mb-3"
+                >
+                    <Form.Control type="email" placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)} />
+                </FloatingLabel>
+
+                <FloatingLabel
+                    controlId="formBasicPassword"
+                    label="Password"
+                    className="mb-3"
+                >
+                    <Form.Control type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} autocomplete="current-password" />
+                </FloatingLabel>
+
+                <div className="d-grid gap-2">
+                    <Button type='submit' disabled={loading}>{loading? "Registering..." : "Register"}</Button>
+                    {message && <p>{message}</p>}
                 </div>
+                </Form>
             </div>
-            <Footer />
-        </>
+            <div className="p-4 box mt-3 text-center">
+                Already have an account? <Link to="/login">Log In</Link>
+            </div>
+        </Container>
     )
 }
 
-export default Register
+export default RegisterForm
